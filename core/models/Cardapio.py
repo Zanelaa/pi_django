0f4c0abbdd18c7472312ec39f7577ce7422048a2
Zanelaa <jorge.zanela19@gmail.com>
2022-10-04 16:54:07 -0300

    relação de id cardapios e refeições

--- core/models/Cardapio.py
+++ core/models/Cardapio.py
@@ -1,12 +1,11 @@
+from ast import Delete
 from django.db import models
 
-from core.models.Refeicoes import Refeicoes
 
 class Cardapio(models.Model):
     titulo_card = models.CharField(max_length=25)
     comentario = models.CharField(max_length=255)
-    refeiçoes = models.ManyToManyField(Refeicoes, related_name="refeiçoes")
 
     def __str__(self):
-        return f'{self.titulo_card} ({self.comentario})'
+        return f'{self.titulo_card} ({self.comentario}) '
         
